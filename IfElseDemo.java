3c485798b3bbf33a6c62b7f80184bf665f0834b8
nehatikore <nehatikore0309@gmail.com>
2025-06-28 18:13:18 +0530

    Add files via upload

---
+++ IfElseDemo.java
@@ -0,0 +1,11 @@
+public class EligibilityCheck {
+    public static void main(String[] args) {
+        int age = 20;  
+
+        if (age >= 18) {
+            System.out.println("Person is eligible to vote.");
+        } else {
+            System.out.println("Person is NOT eligible to vote.");
+        }
+    }
+}
