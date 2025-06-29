311c5ad811f47e1143d16e0c1db833962fd90710
nehatikore <nehatikore0309@gmail.com>
2025-06-29 14:48:57 +0530

    Add files via upload

---
+++ IfDemo.java
@@ -0,0 +1,9 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+    }
+}
