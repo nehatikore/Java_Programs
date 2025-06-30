920de4889890214ac1032c267472be5ce854de98
nehatikore <nehatikore0309@gmail.com>
2025-06-30 16:19:54 +0530

    Add files via upload

---
+++ NonArgument_Constructor.java
@@ -0,0 +1,37 @@
+class Student{
+   
+ 
+    String name;
+    
+     int age;
+    
+   
+    Student()
+    
+     {
+        
+         name="neha";
+        
+        age=10;
+   
+    }
+    
+    void display()
+  
+    {
+       
+         System.out.println("My name is:"+name+"Age:"+age);
+  
+   }
+     
+  public static void main(String[] args)
+ 
+   {
+        
+        Student s1=new Student();
+        
+       s1.display();
+    
+   }
+
+}
