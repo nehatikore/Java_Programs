302419cda88c9e50ce1aa3f256034d35bcf2d1fc
nehatikore <nehatikore0309@gmail.com>
2025-07-01 16:58:59 +0530

    Add files via upload

---
+++ ForEachDemo.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Using foreach loop
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
