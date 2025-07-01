859c2a4fed2a6aba47d559f40c3fa26d22496761
nehatikore <nehatikore0309@gmail.com>
2025-07-01 16:18:04 +0530

    Add files via upload

---
+++ NestedForLoopDemo.java
@@ -0,0 +1,29 @@
+class Main {
+    
+    public static void main(String[] args)
+     {
+       
+           int i;
+       
+           int j;
+        
+          for(i=1;i<=10;i++)
+   
+             { 
+          
+                       //System.out.println(i);
+  
+                      for(j=1;j<=10;j++)
+                        {
+               
+                             System.out.println(i+"*"+j+"="+i*j);
+   
+                        }
+         
+                        System.out.println();
+    
+             }
+   
+     }
+
+}
