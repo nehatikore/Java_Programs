cad6d030a2c7b202d4a71db6b264b5412e9aa4af
nehatikore <nehatikore0309@gmail.com>
2025-07-01 15:46:04 +0530

    Add files via upload

---
+++ ForDemo.java
@@ -0,0 +1,18 @@
+class Main {
+    
+        public static void main(String[] args) 
+              {
+       
+                   int i=1;
+        
+                  for(i=1;i<=10;i++)
+   
+                      {
+           
+                         System.out.println(i);
+   
+                      }
+ 
+            }
+
+}
