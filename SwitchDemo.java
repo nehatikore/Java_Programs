5eb4733704b1c262fdae355a945b27adcab1dd1c
nehatikore <nehatikore0309@gmail.com>
2025-07-02 16:18:34 +0530

    Add files via upload

---
+++ SwitchDemo.java
@@ -0,0 +1,38 @@
+class SwitchDemo {
+ 
+   public static void main(String[] args) 
+       {
+        
+             int i=2;
+        
+            switch(i)
+   
+             {
+            
+                  case 1:
+            
+                         System.out.println("First Choice");
+  
+                         break;
+         
+                 case 2:
+                
+                         System.out.println("Second Choice");
+   
+                         break;
+ 
+               case 3:
+   
+                        System.out.println("Third Choice");
+   
+                        break;
+           
+               default:
+          
+                       System.out.println("Invalid Choice");
+   
+             }
+  
+        }
+
+}
