264fc3f546dad632dab8ee772259a9e89aa47dd1
nehatikore <nehatikore0309@gmail.com>
2025-07-02 16:39:26 +0530

    Add files via upload

---
+++ LocalVariable.java
@@ -0,0 +1,41 @@
+class SwitchDemo {
+    
+  
+      public void addition()
+     
+        {   
+         
+               int x=10;
+      
+               int y=20; //local variable
+ declared inside method
+               int sum ;
+            
+              sum=x+y;
+            
+              System.out.println(sum);
+            
+            //if we type public int addition(),then use return for display.
+       
+           //int sum;   
+          
+          //sum=x+y;
+            
+          //System.out.println(sum);
+        
+           //return sum;
+    
+    }
+
+        
+  public static void main(String[] args) 
+    {
+            
+           
+               SwitchDemo obj=new SwitchDemo();
+    
+              obj.addition();
+  
+   }  
+
+}
