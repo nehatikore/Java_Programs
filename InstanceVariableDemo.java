ed7df2206ab4cabd21caf72fefdb5cec5d8b1efd
nehatikore <nehatikore0309@gmail.com>
2025-07-03 17:27:30 +0530

    Add files via upload

---
+++ InstanceVariableDemo.java
@@ -0,0 +1,30 @@
+class InstanceVariableDemo {
+  
+             String name;
+ 
+             int age;
+        
+             public void display()
+    
+               {  
+           
+                    System.out.println(name);
+   
+       
+        }
+
+     
+     public static void main(String[] args) 
+         {
+    
+        
+            InstanceVariableDemo obj=new InstanceVariableDemo();
+                                obj.name="neha";
+      
+                    obj.display();
+ 
+                    System.out.println("heyy");
+   
+      }
+
+}
